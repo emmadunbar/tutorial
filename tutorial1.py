51504c95d2d20cb32d0ec385bc8a9054fe716a13
emmadunbar <42850826+emmadunbar@users.noreply.github.com>
2018-09-04 12:41:13 -0400

    Updated from Brython Server: 9/4/2018 12:41:12 PM

--- tutorial1.py
+++ tutorial1.py
@@ -1,3 +1,19 @@
 from ggame import App
+
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+
+red=Color(0xff0000,1.0)
+green=Color(0x00ff00,1.0)
+blue=Color(0x0000ff,1.0)
+black=Color(0x000000,1.0)
+
+tline=LineStyle(1, black)
+rectangle=RectangleAsset(50,50,tline,red)
+
+Sprite(rectangle)
+
 myapp = App()
 myapp.run()
